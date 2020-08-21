1758df6efa6b2e61fd6cf319c288bd67285d31b3
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-21 13:05:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,22 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  data = YAML.load_file(file)
+  result = {}
+    data.each_pair do |key, value|
+      result[key] = {:english => value[0], :japanese => value[1]}
+    end
+   #binding.pry
+  return result
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, japanese_emoticon)
+  result = load_library(file)
+  result.each_pair do |mood, language|
+    if result[mood][:japanese] == japanese_emoticon
+      return result[mood]
+    end
+  end
+  #binding.pry
 end
